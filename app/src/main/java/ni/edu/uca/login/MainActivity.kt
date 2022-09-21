fabd459bdfb6b2b7f7b035e764fcfc3045393fde
gabrielMerino <gabriel.merino13079@est.uca.edu.ni>
2022-09-21 14:12:21 -0600

    La clase de 21/9/2022 android studio

---
+++ app/src/main/java/ni/edu/uca/login/MainActivity.kt
@@ -0,0 +1,38 @@
+package ni.edu.uca.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import ni.edu.uca.login.databinding.ActivityMainBinding
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding;
+    val codigo:String = "Gabriel";
+    val password:String = "1234";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+        iniciar();
+    }
+
+    private fun iniciar() {
+    binding.btnOk.setOnClickListener {
+        val id: String = binding.etUsuario.text.toString();
+        val pwd: String = binding.etPwd.text.toString();
+        validarCredenciales(id, pwd)
+    }
+    }
+
+    private fun validarCredenciales(id: String, pwd: String) {
+        if(id.equals(codigo) && pwd.equals(password)) {
+            Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this, "Error en credenciales", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
